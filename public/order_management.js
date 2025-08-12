4af270d2547b60057b0177d2b990f6afa6125fbf
nekko-daisuki <kkht0805@icloud.com>
2025-08-12 20:55:43 +0900

    Update order_management.js

--- public/order_management.js
+++ public/order_management.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const filterButtons = document.querySelectorAll('.filter-btn');
 
     // !!! ここをあなたのデプロイURLに置き換えてください !!!
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzywAJqYxhOoaRRaukUUQI9Ti2Lkz67sBsgfiD-H0Jzks6Fdvgrsii2AtzoF_jP9buY/exec';
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxa7lbZFEcWhcGy0S_HDRErB6yHDbxXtCP7k2TchGc12jokBcNCWFb9b-DifMeOWm8X/exec';
 
     let allOrders = [];
     let currentFilter = 'all';

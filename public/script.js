44816f14535822d830da1773e4b61ce1f2948758
nekko-daisuki <kkht0805@icloud.com>
2025-08-12 21:26:20 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Google Apps ScriptのデプロイURL
     // !!! ここをあなたのデプロイURLに置き換えてください !!!
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxa7lbZFEcWhcGy0S_HDRErB6yHDbxXtCP7k2TchGc12jokBcNCWFb9b-DifMeOWm8X/exec'; 
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw5--OG693b0upqbwQPrQeH4zIaGEZlYoFzEZU6lk8sPYS6zKPH1GMWJQBYjqX2YPBs/exec'; 
 
     async function loadMenuItems() {
         try {
